fe8153b001e33cc930e446b9002a2f93668cc63a
SakpalAkshay <akki.sakp01@protonmail.com>
2025-04-06 23:27:47 -0700

    Pushed Portfolio Code

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 // next.config.js
 module.exports = {
-    output: 'export',
-    // Optional: Add other configurations if needed
+    output: 'export',  // Ensure this is set to export static files
+    basePath: '/akshaysakpal.github.io',  // Use your repository name (e.g., '/akshaysakpal.github.io')
+    assetPrefix: '/akshaysakpal.github.io/',  // Prefix for static assets
+    // Optional: Other Next.js configurations you might need
   }
   
